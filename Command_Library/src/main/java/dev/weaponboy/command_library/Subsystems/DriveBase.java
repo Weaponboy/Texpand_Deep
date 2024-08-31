6fa91d13fae9e8d4b6a79c17349431dfb1ddec97
isa <isaadams54@gmail.com>
2024-08-31 14:17:18 +0200

    getting new sdk

--- Command_Library/src/main/java/dev/weaponboy/command_library/Subsystems/DriveBase.java
+++ Command_Library/src/main/java/dev/weaponboy/command_library/Subsystems/DriveBase.java
@@ -1,17 +1,15 @@
-package dev.weaponboy.command_library.Subsystems;
+package dev.weaponboy.command_library.Examples.Subsystems;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
-import dev.weaponboy.command_library.CommandLibrary.Commands.Command;
-import dev.weaponboy.command_library.CommandLibrary.Commands.LambdaCommand;
-import dev.weaponboy.command_library.CommandLibrary.OpmodeEX.OpModeEX;
-import dev.weaponboy.command_library.CommandLibrary.Subsystem.SubSystem;
+import dev.weaponboy.command_library.Commands.LambdaCommand;
+import dev.weaponboy.command_library.OpmodeEX.OpModeEX;
+import dev.weaponboy.command_library.Subsystem.SubSystem;
 
 public class DriveBase extends SubSystem {
 
     DcMotorEx slideMotor;
-    double vertical;
 
     public DriveBase(OpModeEX opModeEX){
         registerSubsystem(opModeEX, PIDControl);
@@ -27,20 +25,14 @@ public class DriveBase extends SubSystem {
         executeEX();
     }
 
-    public Command drivePower(double vertical, double horizontal, double pivot){
-        this.vertical = vertical;
-        return PIDControl;
-    }
-
-    private LambdaCommand PIDControl = new LambdaCommand(
+   public LambdaCommand PIDControl = new LambdaCommand(
             () -> {
                 slideMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
                 slideMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
             },
             () -> slideMotor.setPower(0.5),
             () -> slideMotor.getCurrentPosition() > 2000
+
     );
 
-
 }
-
