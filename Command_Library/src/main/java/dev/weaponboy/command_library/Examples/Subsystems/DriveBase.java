66c6f45d1de8527cd7397090fff949dd7e56966c
Weaponboy <joshandy06@gmail.com>
2024-08-31 14:39:37 -0500

    merge resolved

---
+++ Command_Library/src/main/java/dev/weaponboy/command_library/Examples/Subsystems/DriveBase.java
@@ -0,0 +1,38 @@
+package dev.weaponboy.command_library.Examples.Subsystems;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import dev.weaponboy.command_library.CommandLibrary.Commands.LambdaCommand;
+import dev.weaponboy.command_library.CommandLibrary.OpmodeEX.OpModeEX;
+import dev.weaponboy.command_library.CommandLibrary.Subsystem.SubSystem;
+
+public class DriveBase extends SubSystem {
+
+    DcMotorEx slideMotor;
+
+    public DriveBase(OpModeEX opModeEX){
+        registerSubsystem(opModeEX, PIDControl);
+    }
+
+    @Override
+    public void init() {
+        slideMotor = getOpModeEX().hardwareMap.get(DcMotorEx.class, "motor1");
+    }
+
+    @Override
+    public void execute() {
+        executeEX();
+    }
+
+   public LambdaCommand PIDControl = new LambdaCommand(
+            () -> {
+                slideMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                slideMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            },
+            () -> slideMotor.setPower(0.5),
+            () -> slideMotor.getCurrentPosition() > 2000
+
+    );
+
+}
