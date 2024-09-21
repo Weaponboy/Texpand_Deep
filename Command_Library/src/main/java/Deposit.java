9f6f3fbcb4a54e43014a7f374d57698e22bcd19e
isa <isaadams54@gmail.com>
2024-09-21 12:01:49 +0200

    commit

---
+++ Command_Library/src/main/java/Deposit.java
@@ -0,0 +1,66 @@
+import dev.weaponboy.command_library.CommandLibrary.Commands.LambdaCommand;
+import dev.weaponboy.command_library.CommandLibrary.Subsystem.SubSystem;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import dev.weaponboy.command_library.CommandLibrary.Commands.LambdaCommand;
+import dev.weaponboy.command_library.CommandLibrary.Subsystem.SubSystem;
+
+
+public class Deposit extends SubSystem {
+    DcMotor deposite1;
+    Servo depo1;
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void execute() {
+
+    }
+
+    public LambdaCommand deposithigh = new LambdaCommand(
+            () ->
+
+            () ->
+
+            () ->
+
+    );
+    public LambdaCommand depositeLow = new LambdaCommand(
+
+            () ->
+
+
+            () ->
+
+            () ->
+
+    );
+
+
+    public LambdaCommand stow = new LambdaCommand(
+            () ->
+
+            () ->
+
+            () ->
+
+
+    );
+
+}
+
+
+
+
+
+
+
+
+
+
+
