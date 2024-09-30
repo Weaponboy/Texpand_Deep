b515a2373bba2e298484184f15044afd8c6eb592
mcbuc <joshandy06@gmail.com>
2024-09-30 19:52:59 +0200

    path points added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Blue_Left.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Blue_Left.java
@@ -10,13 +10,20 @@ import dev.weaponboy.nexus_pathing.PathingUtility.RobotPower;
 import dev.weaponboy.nexus_pathing.RobotUtilities.Vector2D;
 
 @Autonomous(name = "testing_pathing", group = "Test Autos")
-public class ExampleAuto extends OpModeEX {
+public class Blue_Left extends OpModeEX {
 
     pathsManager paths = new pathsManager();
     follower follow = new follower();
 
-    private final sectionBuilder[] preloadPath = {
-            () -> paths.addPoints(new Vector2D(341, 210), new Vector2D(331, 268), new Vector2D(336, 328))
+    private final sectionBuilder[] preloadPath = new sectionBuilder[]{
+            () -> paths.addPoints(new Vector2D(271, 335), new Vector2D(308.5, 332.3), new Vector2D(335.7, 335.3)),
+            () -> paths.addPoints(new Vector2D(335.7, 335.3), new Vector2D(315.4, 300), new Vector2D(303.6, 267)),
+            () -> paths.addPoints(new Vector2D(303.6, 267), new Vector2D(315.4, 300), new Vector2D(335.7, 335.3)),
+            () -> paths.addPoints(new Vector2D(335.7, 335.3), new Vector2D(333.7, 311.7), new Vector2D(330.2, 267)),
+            () -> paths.addPoints(new Vector2D(330.2, 267), new Vector2D(333.7, 311.7), new Vector2D(335.7, 335.3)),
+            () -> paths.addPoints(new Vector2D(335.7, 335.3), new Vector2D(303, 256.4), new Vector2D(331.2, 246.7)),
+            () -> paths.addPoints(new Vector2D(331.2, 246.7), new Vector2D(303, 256.4), new Vector2D(335.7, 335.3)),
+
     };
 
     @Override
