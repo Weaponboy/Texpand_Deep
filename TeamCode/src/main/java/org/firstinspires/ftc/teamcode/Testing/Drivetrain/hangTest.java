54d3b2a4398f08a6fe729e719924de947609df06
mcbuc <joshandy06@gmail.com>
2024-10-09 14:05:42 +0200

    added hang test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/Drivetrain/hangTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/Drivetrain/hangTest.java
@@ -3,29 +3,37 @@ package org.firstinspires.ftc.teamcode.Testing.Drivetrain;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import dev.weaponboy.command_library.CommandLibrary.OpmodeEX.OpModeEX;
 @TeleOp
 public class hangTest extends OpMode {
-    CRServo hang1;
-    CRServo hang2;
+    Servo hang1;
+    Servo hang2;
     @Override
     public void init() {
-        hang1=hardwareMap.get(CRServo.class,"hang1");
-        hang2=hardwareMap.get(CRServo.class,"hang2");
+        hang1=hardwareMap.get(Servo.class,"hang1");
+        hang2=hardwareMap.get(Servo.class,"hang2");
+//        hang1.setDirection(Servo.Direction.REVERSE);
 
     }
 
     @Override
     public void loop() {
         if (gamepad1.y){
-        hang1.setPower(0);
-        hang2.setPower(0);
+            hang1.setPosition(1);
+            hang2.setPosition(1);
+
+        }else if (gamepad1.b){
+            hang1.setPosition(0);
+            hang2.setPosition(0);
 
         }else {
-            hang1.setPower(0.5);
-            hang2.setPower(0.5);
+            hang1.setPosition(0.5);
+            hang2.setPosition(0.5);
         }
+        telemetry.addData("pos", hang1.getPosition());
+
     }
 }
