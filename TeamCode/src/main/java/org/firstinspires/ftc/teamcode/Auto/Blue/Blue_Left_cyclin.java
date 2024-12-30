c88e6cc203beb8758a130705c82fd53e67339b96
Weaponboy <joshandy06@gmail.com>
2024-12-30 08:57:42 -0500

    Heading update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Blue/Blue_Left_cyclin.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Blue/Blue_Left_cyclin.java
@@ -611,7 +611,7 @@ public class Blue_Left_cyclin extends OpModeEX {
 
                 follow.setPath(paths.returnPath("dropBasket"));
 
-                targetHeading = 205;
+                targetHeading = 190;
 
                 follow.usePathHeadings(false);
 
