3e94bf48117ace360c7b424e65c9f5c7595a9f13
Weaponboy <joshandy06@gmail.com>
2024-12-31 10:43:40 -0500

    Vision change to actually detects

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Blue/Blue_Left_cyclin.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Blue/Blue_Left_cyclin.java
@@ -97,7 +97,7 @@ public class Blue_Left_cyclin extends OpModeEX {
     };
 
     private final sectionBuilder[] subCollect = new sectionBuilder[]{
-            () -> paths.addPoints(new Vector2D(326.3, 326), new Vector2D(204, 288), new Vector2D(200, 238)),
+            () -> paths.addPoints(new Vector2D(326.3, 326), new Vector2D(204, 288), new Vector2D(200, 244)),
     };
 
     private final sectionBuilder[] spikeDeposit = new sectionBuilder[]{
