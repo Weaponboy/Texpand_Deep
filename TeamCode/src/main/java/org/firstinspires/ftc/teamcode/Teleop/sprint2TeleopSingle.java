c531620827681f873ea91267e1b4106da5476418
Weaponboy <joshandy06@gmail.com>
2025-01-03 07:50:02 -0500

    Removed auto transfer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/sprint2TeleopSingle.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/sprint2TeleopSingle.java
@@ -263,10 +263,6 @@ public class sprint2TeleopSingle extends OpModeEX {
             delivery.runReset();
         }
 
-        if(collection.getFourBarState() == Collection.fourBar.collect && collection.clawSensor.isPressed() && collection.getClawsState() == Collection.clawState.drop && !queueCollection){
-            queueCollection = true;
-        }
-
         if (queueCollection && collection.getCurrentCommand() == collection.defaultCommand && collection.getFourBarState() == Collection.fourBar.collect){
 
             collection.queueCommand(collection.transfer);
---
+++ Vision/src/main/java/dev/weaponboy/vision/Archived/Get_Encoder_Positions.java
@@ -0,0 +1,36 @@
+package dev.weaponboy.vision.Archived;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp
+public class Get_Encoder_Positions extends OpMode {
+
+    DcMotor arm_Motor;
+    DcMotor wrist_Motor;
+
+    @Override
+    public void init() {
+
+        arm_Motor = hardwareMap.get(DcMotor.class,"arm_Motor");
+        wrist_Motor = hardwareMap.get(DcMotor.class,"wrist_Motor");
+
+        arm_Motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        arm_Motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        wrist_Motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        wrist_Motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addData("Arm position", arm_Motor.getCurrentPosition());
+        telemetry.addData("Wrist position", wrist_Motor.getCurrentPosition());
+        telemetry.update();
+    }
+}
