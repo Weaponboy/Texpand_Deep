9beec7c2965c40ec5f40b510f9b749e127801eb0
Weaponboy <joshandy06@gmail.com>
2025-01-07 04:23:13 -0500

    Slight release thingy

--- Command_Library/src/main/java/dev/weaponboy/command_library/Subsystems/Collection.java
+++ Command_Library/src/main/java/dev/weaponboy/command_library/Subsystems/Collection.java
@@ -187,8 +187,8 @@ public class Collection extends SubSystem {
     /**
      * stowed position values
      * */
-    double mainPivotTransfer = 210;
-    double secondPivotTransfer = 110;
+    double mainPivotTransfer = 195;
+    double secondPivotTransfer = 140;
     double rotateTransfer = 180;
 
     /**
